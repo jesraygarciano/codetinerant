7f2b20ff57352ac05c77f2489ffae14c0bf1c6d6
Jesray Garciano <jesray.garciano@gmail.com>
2018-02-17 16:53:59 +0800

    Adding authentication

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
+        <title>{{ config('app.name','Codetinerant') }}</title>
+        <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
@@ -65,31 +65,32 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+            <header class="v-header container">
+                <div class="fullscreen-video-wrap">
+                    <video src="vid/understang.mp4" autoplay="true" loop="true"></video>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+                <div class="header-overlay"></div>
+
+                    <div class="header-content">
+                        <h1>Jesray Garciano</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ab delectus commodi dolores quaerat deleniti dicta rem labore reiciendis laborum quasi eius amet atque facilis, soluta culpa quae, vero quidem.</p>
+                        <a href="#" class="btn">Read more!</a>
+                    </div>
+
+            </header>
+
+            <section class="section">
+                <h1>Section One</h1>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere dicta, dolores aspernatur veritatis. Quia, ipsam ex odit delectus qui, quis modi, voluptate, similique deleniti ut quisquam suscipit explicabo aspernatur neque.
+            </section>
+
+            <section class="section section-btm">
+                <h1>Section Two</h1>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro repudiandae laboriosam commodi, fuga explicabo neque esse eveniet voluptatum consequuntur eum quisquam facere provident cumque voluptas dolor ducimus voluptate ex iure totam, maxime blanditiis est similique id expedita. Beatae perferendis eius, animi est, praesentium ab illo odit neque, aspernatur facere excepturi.
+
+            </section>
+
+<script src="js/app.js"></script>
+</body>
 </html>
